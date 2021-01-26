4495bed51a4e4219cdfc13c48e4e89bee820d64e
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-01-26 19:33:38 +0100

    ehmm stiekem een wall baunce gemaakt

--- WalkingSim/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ WalkingSim/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -15,9 +15,9 @@ public class PlayerMovement : MonoBehaviour
     public float crouchHeight, bodyHeight, slideHeight;
     public Vector3 centerNeutral, centerCrouch, centerSlide;
     ///private
-    private Vector3 moveDir;
+    private Vector3 moveDir,lastMove,dubbelLastMove;
     private float speed;
-    private bool sliding,isCrouching;
+    private bool sliding,isCrouching,dubbeljumping;
 
     private int jumpcount = 1;
 
@@ -48,9 +48,14 @@ public class PlayerMovement : MonoBehaviour
                 isCrouching = !isCrouching;
                 Crouch(isCrouching);
             }
-        }        
+        }
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.forward = new Vector3(cam.transform.forward.x, transform.forward.y, cam.transform.forward.z);
+        moveDir = transform.TransformDirection(moveDir);
+
         if (controller.isGrounded)
         {
+            dubbeljumping = false;
             if (Time.time == groundedCooldown + Time.time)
             {
                 downForce = -0.01f;
@@ -69,25 +74,48 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            downForce -= gravity * Time.deltaTime;
-            //jump
-            if (Input.GetButtonDown("Jump"))
+            if (!dubbeljumping)
             {
-                if (jumpcount > 0)
-                {
-                    ResetAnim();
-                    anim.SetBool("IsJumping", true);
-                    Invoke("ResetAnim", 1);
-                    jumpcount--;
-                    downForce = jumpforce;
-                }
+            moveDir = lastMove;
             }
+            downForce -= gravity * Time.deltaTime;
+            //dubbeljump
+            //if (Input.GetButtonDown("Jump"))
+            //{
+            //    if (jumpcount > 0)
+            //    {
+            //        dubbeljumping = true;
+            //        ResetAnim();
+            //        anim.SetBool("IsJumping", true);
+            //        Invoke("ResetAnim", 1);
+            //        jumpcount--;
+            //        downForce = jumpforce;
+            //        moveDir = dubbelLastMove;
+            //        print("2");
+            //    }
+            //}
         }
-        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.forward = new Vector3(cam.transform.forward.x, transform.forward.y, cam.transform.forward.z);
-        moveDir = transform.TransformDirection(moveDir);
         controller.Move(moveDir.normalized * speed * Time.deltaTime);
         controller.Move(new Vector3(0, downForce, 0) * walkingspeed * Time.deltaTime);
+        lastMove = moveDir;
+        if (dubbeljumping)
+        {
+            dubbelLastMove = moveDir;
+        }
+    }
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (!controller.isGrounded&& hit.gameObject.layer == 10)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                ResetAnim();
+                anim.SetBool("IsJumping", true);
+                Invoke("ResetAnim", 1);
+                downForce = jumpforce;
+                moveDir = hit.normal * speed;
+            }
+        }
     }
     public void Crouch(bool crouching)
     {
