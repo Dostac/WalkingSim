4451f84108fc6f62279abf351085440623a767a3
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-01-27 04:27:24 +0100

    begin walrun

--- WalkingSim/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ WalkingSim/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -14,17 +14,55 @@ public class PlayerMovement : MonoBehaviour
     public Animator anim;
     public float crouchHeight, bodyHeight, slideHeight;
     public Vector3 centerNeutral, centerCrouch, centerSlide;
+    public Transform orientation;
+    public float wallrunForce=3000;
+    public float wallGrafity;
+    public LayerMask wall;
     ///private
     private Vector3 moveDir,lastMove,dubbelLastMove;
     private float speed;
-    private bool sliding,isCrouching,dubbeljumping;
+    private bool sliding,isCrouching,dubbeljumping, isWallRight, isWallLeft,isWallRunning;
 
     private int jumpcount = 1;
 
     void Update()
     {
         //UpdateUpdate();
+        CheckForWall();
+        WallRunInput();
         InPut();
+    }
+    private void WallRunInput() 
+    {
+        //Wallrun
+        if (Input.GetKey(KeyCode.D) && isWallRight) StartWallrun();
+        if (Input.GetKey(KeyCode.A) && isWallLeft) StartWallrun();
+    }
+    private void StartWallrun()
+    {
+        print("start");
+
+        isWallRunning = true;
+
+
+            //Make sure char sticks to wall
+           // if (isWallRight)
+               // rb.AddForce(orientation.right * wallrunForce / 5 * Time.deltaTime);
+           // else
+               // rb.AddForce(-orientation.right * wallrunForce / 5 * Time.deltaTime);
+        //}
+    }
+    private void StopWallRun()
+    {
+        isWallRunning = false;        
+    }
+    private void CheckForWall() //make sure to call in void Update
+    {
+        isWallRight = Physics.Raycast(transform.position, orientation.right, 1f, wall);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.right, 1f, wall);
+
+        //leave wall run
+        if (!isWallLeft && !isWallRight) StopWallRun();
     }
     private void InPut()
     {
@@ -51,8 +89,15 @@ public class PlayerMovement : MonoBehaviour
         }
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         transform.forward = new Vector3(cam.transform.forward.x, transform.forward.y, cam.transform.forward.z);
+        if (isWallRight&&isWallRunning)
+        {
+            transform.rotation = Quaternion.Euler(cam.transform.forward.x, transform.forward.y, 15);
+        }
+        if (isWallLeft&& isWallRunning)
+        {
+            transform.rotation = Quaternion.Euler(cam.transform.forward.x, transform.forward.y, -15);
+        }
         moveDir = transform.TransformDirection(moveDir);
-
         if (controller.isGrounded)
         {
             dubbeljumping = false;
