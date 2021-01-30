e40603e587f2f8fd4941fb2f995c223675231145
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-01-30 18:51:37 +0100

    checkpoint system
    oude movment update maar vooral gaat deze push om de checkpoints

---
+++ WalkingSim/Assets/Scripts/Game Functionality/checkpoint Script/CheckPoint.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CheckPoint : MonoBehaviour
+{ 
+    //public
+    public CheckPointManager cpm;
+    public bool canBeTriggert=false;
+    //private
+    private bool achieved;
+    public void Add()
+    {
+        cpm.index++;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (canBeTriggert)
+        {
+            if (!achieved && other.gameObject.tag == "Player")
+            {
+                Add();
+                achieved = false;
+            }
+        }
+    }
+}
