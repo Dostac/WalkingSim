9176de15da67fd86b8ec55063d38225010503f1c
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-02-12 10:48:59 +0100

    respawn point

---
+++ WalkingSim/Assets/Scripts/Game Functionality/checkpoint Script/ResetPointScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+public class ResetPointScript : MonoBehaviour
+{
+    //public
+    public Vector3 spawnPos;
+    //private
+    private float posX, posY, posZ;
+    private GameObject player;
+
+    public void OnCheckPoint()
+    {
+        SetPosition();
+        SavePos();
+        GetPos();
+        Respawn();
+    }
+    private void Awake()
+    {
+        player= GameObject.FindGameObjectWithTag("Player");
+
+        posX = PlayerPrefs.GetFloat("posX");
+        posY = PlayerPrefs.GetFloat("posY");
+        posZ = PlayerPrefs.GetFloat("posZ");
+
+        spawnPos = new Vector3(posX, posY, posZ);
+    }
+    public void SetPosition()
+    {
+        spawnPos = player.transform.position;
+
+        posX = spawnPos.x;
+        posY = spawnPos.y;
+        posZ = spawnPos.z;
+    }
+    public void SavePos()
+    {
+        PlayerPrefs.SetFloat("posX", posX);
+        PlayerPrefs.SetFloat("posY", posY);
+        PlayerPrefs.SetFloat("posZ", posZ);
+    }
+   public void GetPos()
+    {
+       posX = PlayerPrefs.GetFloat("posX");
+       posY = PlayerPrefs.GetFloat("posY");
+       posZ = PlayerPrefs.GetFloat("posZ");
+    }
+    public void Respawn()
+    {
+        spawnPos = new Vector3(posX, posY, posZ);
+        transform.position = spawnPos;
+    }
+
+    void OnApplicationQuit()
+    {
+        SavePos();
+    }
+} 
